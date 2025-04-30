d61d36b327fd2fa0751b531ae346e5ec612e36ee
Armands <a.rancevs@gmail.com>
2025-04-30 20:31:32 +0300

    Add backend deps to frontend package.json

--- rental-platform-frontend/netlify/functions/api.js
+++ rental-platform-frontend/netlify/functions/api.js
@@ -1,7 +1,4 @@
+// rental-platform-frontend/netlify/functions/api.js
 const serverless = require("serverless-http");
-// point this at the file that *exports* your Express app, not the one that calls app.listen()
-const app = require("../../server")  
-
-// serverless-http will take all your routes (/admin, /api, etc.) 
-// and turn them into a single Netlify Function.
+const app = require("./server");   // now server.js is next door
 module.exports.handler = serverless(app);
